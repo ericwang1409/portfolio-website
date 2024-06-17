a7b401ca6210a822a4075da7ecb2675163aa0ecd
Eric <eric3691215@gmail.com>
2024-06-16 22:53:24 -0400

    picture

--- portfolio-website/src/app/navbar.tsx
+++ portfolio-website/src/app/navbar.tsx
@@ -1,24 +1,70 @@
-import React from "react";
+import React from 'react'
 
 const Navbar = () => {
     return (
         <>
-        <nav className="bg-lighter-beige">
-        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-16">
-                <div className="flex items-center">
-                    <div className="flex-shrink-0 space-x-16">
-                        <a href="/" className="text-black font-mono">Eric Wang</a>
-                        <a href="/" className="text-black font-mono">Projects</a>
-                        <a href="/" className="text-black font-mono">Resume</a>
-                        <a href="/" className="text-black font-mono">Blog</a>
+            <nav className="bg-lighter-beige">
+                <div className="flex justify-center items-center h-16">
+                    <div className="flex gap-x-8">
+                        <div>
+                            <a href="/" className="text-black font-mono">
+                                Eric Wang
+                            </a>
+                        </div>
+                        <div>
+                            <a href="/" className="text-black font-mono">
+                                Projects
+                            </a>
+                        </div>
+                        <div>
+                            <a href="/" className="text-black font-mono">
+                                Resume
+                            </a>
+                        </div>
+                        <div>
+                            <a href="/" className="text-black font-mono">
+                                Blog
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        </nav>
+                {/* <div className="flex justify-center space-x-16 h-10 items-center px-8">
+                    <a href="/" className="text-black font-mono">
+                        Eric Wang
+                    </a>
+                    <a href="/" className="text-black font-mono">
+                        Projects
+                    </a>
+                    <a href="/" className="text-black font-mono">
+                        Resume
+                    </a>
+                    <a href="/" className="text-black font-mono">
+                        Blog
+                    </a>
+                </div> */}
+                {/* <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex items-center justify-between h-16">
+                        <div className="flex items-center">
+                            <div className="flex-shrink-0 space-x-16">
+                                <a href="/" className="text-black font-mono">
+                                    Eric Wang
+                                </a>
+                                <a href="/" className="text-black font-mono">
+                                    Projects
+                                </a>
+                                <a href="/" className="text-black font-mono">
+                                    Resume
+                                </a>
+                                <a href="/" className="text-black font-mono">
+                                    Blog
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+            </nav>
         </>
-    );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -1,19 +1,60 @@
-import Image from "next/image";
-import Navbar from "./navbar";
+import Image from 'next/image'
+import Navbar from './navbar'
 
 export default function Home() {
-  return (
-    <main>
-      <header>
-        <Navbar/>
-      </header>
-      
-      <section className="bg-gradient-radial h-screen bg-beige">
-        <div className="flex flex-col items-center justify-center h-full">
-          <h1 className="text-4xl font-bold text-black font-mono">Hello, world!</h1>
-          <p className="text-lg text-black font-mono">Welcome to my portfolio website.</p>
-        </div>
-      </section>
-    </main>
-  );
+    return (
+        <main>
+            <Navbar />
+            <div className="flex justify-center mt-3 mb-8">
+                <Image
+                    src="/eric-wang.jpg"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                    className="rounded-lg"
+                />
+            </div>
+            <div className="flex justify-center">
+                <div
+                    className="bg-neutral-200 flex items-center justify-center font-mono px-4 py-6 rounded-lg shadow max-w-xl mx-auto"
+                    style={{
+                        minHeight: '4rem',
+                        lineHeight: '1.5',
+                        textAlign: 'center',
+                    }}
+                >
+                    Hi! I am an undergrad at Harvard studying Computer Science
+                    and Math.
+                </div>
+            </div>
+        </main>
+    )
+}
+
+{
+    /* <div className="grid grid-cols-4 grid-rows-2 gap-4">
+                <div className="flex items-center justify-center">01</div>
+                <div className="flex items-center justify-center col-span-2"></div>
+                <div className="flex items-center justify-center">04</div>
+                <div className="flex items-center justify-center">05</div>
+                <div className="flex items-center justify-center col-span-2">
+                    06
+                </div>
+                <div className="flex items-center justify-center">08</div>
+            </div> */
+}
+{
+    /* <header>
+                <Navbar />
+            </header>
+            <section className="bg-gradient-radial h-screen bg-beige">
+                <div className="flex flex-col items-center justify-center h-full">
+                    <h1 className="text-4xl font-bold text-black font-mono">
+                        Hello, world!
+                    </h1>
+                    <p className="text-lg text-black font-mono">
+                        Welcome to my portfolio website.
+                    </p>
+                </div>
+            </section> */
 }
